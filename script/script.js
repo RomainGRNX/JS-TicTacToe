69eee306f4e65a0a588339fd543628bf223f1552
RomainGRNX <romaingarenaux@gmail.com>
2017-12-21 16:19:04 +0100

    TicTacToe v1.0

--- script/script.js
+++ script/script.js
@@ -10,6 +10,46 @@ var current_player = "player1";
 var step = 1;
 var winner = "";
 
+var scores = [0,0];
+
+var view_scores = false;
+var score_button = document.getElementById("scores-button");
+score_button.onclick = function(){
+  if(!end && !view_scores){
+    view_scores = true;
+    var scoresDiv = document.createElement("div");
+    scoresDiv.id = "scores";
+    scoresDiv.className = "text";
+    var score1 = document.createElement("p");
+    var msg1 = document.createTextNode("Player 1: "+scores[0]);
+    score1.appendChild(msg1);
+    scoresDiv.appendChild(score1);
+    var score2 = document.createElement("p");
+    var msg2 = document.createTextNode("Player 2: "+scores[1]);
+    score2.appendChild(msg2);
+    scoresDiv.appendChild(score2);
+    document.body.appendChild(scoresDiv);
+    document.getElementById("scores").addEventListener("click", backToGame, false);
+  }
+};
+
+var give_up_button = document.getElementById("give-up-button");
+give_up_button.onclick = function(){
+  if(!end && !view_scores){
+    end = true;
+    console.log("Le player "+(current_player == "player1" ? 1 : 2)+" a abandonné!!!");
+    scores[0] += (current_player == "player2" ? (11-step)*10 : 0);
+    scores[1] += (current_player == "player1" ? (11-step)*10 : 0);
+    setEndMsg("Le player "+(current_player == "player1" ? 2 : 1)+" gagne!");
+  }
+};
+
+function backToGame(){
+  view_scores = false;
+  var scoresMsg = document.getElementById("scores");
+  scoresMsg.parentNode.removeChild(scoresMsg);
+}
+
 window.addEventListener("load", function(){setInterval(update, 10);}, false);
 function update(){
   if(!end){
@@ -23,7 +63,7 @@ function update(){
 }
 
 function cellClick(){
-  if(!end){
+  if(!end && !view_scores){
     if(this.className == ""){
       console.log("Tour: "+step+" / Cellule "+(getCellIndex(this)+getRowIndex(this)*3)+" cliquée par le joueur "+(current_player == "player1" ? 1 : 2));
       this.className = current_player;
@@ -33,12 +73,26 @@ function cellClick(){
       if(win()){
         end = true;
         winner = checkWinner();
+        scores[0] += (winner == "player1" ? (11-step)*10 : 0);
+        scores[1] += (winner == "player2" ? (11-step)*10 : 0);
+        setEndMsg("Le player "+(winner == "player1" ? 1 : 2)+" gagne!");
       }
       else if(step > 9){
         end = true;
+        setEndMsg("Match nul!");
       }
     }
   }
+}
+
+function setEndMsg(p_msg){
+  var msgDiv = document.createElement("div");
+  msgDiv.id = "end-message";
+  msgDiv.className = "text endAlert";
+  var msg = document.createTextNode(p_msg+"\nNouvelle partie?");
+  msgDiv.appendChild(msg);
+  document.body.appendChild(msgDiv);
+  document.getElementById("end-message").addEventListener("click", resetGame, false);
 }
 
 function checkWinner(){
@@ -97,6 +151,11 @@ function resetGame(){
     }
   }
   end = false;
-  current_player = "player1";
+  current_player = (scores[0] > scores[1] ? "player2" : "player1");
+  step = 1;
   winner = "";
+  var endMsg = document.getElementById("end-message");
+  endMsg.parentNode.removeChild(endMsg);
+  console.log("Score actuel -> Player 1: "+scores[0]+" / Player 2: "+scores[1]);
+  console.log("Nouvelle partie...");
 }
